1e694b776f15966cb273024e4ba8c713443b8286
ronan <321393295@qq.com>
2024-02-26 22:00:57 +0800

    添加网关服务

---
+++ xuecheng-plus-gateway/src/main/java/com/xuecheng/GatewayApplication.java
@@ -0,0 +1,20 @@
+package com.xuecheng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * ClassName: GatewayApplication
+ * Package: com.xuecheng
+ * Description:
+ *
+ * @Author: Ronan
+ * @Create 2024/2/26 - 21:18
+ * @Version: v1.0
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
