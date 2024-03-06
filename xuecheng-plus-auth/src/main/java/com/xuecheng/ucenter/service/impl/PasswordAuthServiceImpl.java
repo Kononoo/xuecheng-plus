14bc784c3839915e2fbe1afdf8bd1d752f6b9994
ronan <321393295@qq.com>
2024-03-06 21:30:58 +0800

    导入验证码功能模块

--- xuecheng-plus-auth/src/main/java/com/xuecheng/ucenter/service/impl/PasswordAuthServiceImpl.java
+++ xuecheng-plus-auth/src/main/java/com/xuecheng/ucenter/service/impl/PasswordAuthServiceImpl.java
@@ -1,13 +1,12 @@
 package com.xuecheng.ucenter.service.impl;
 
-import com.baomidou.mybatisplus.extension.conditions.query.LambdaQueryChainWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.xuecheng.ucenter.mapper.XcUserMapper;
 import com.xuecheng.ucenter.model.dto.AuthParamsDto;
 import com.xuecheng.ucenter.model.dto.XcUserExt;
 import com.xuecheng.ucenter.model.po.XcUser;
 import com.xuecheng.ucenter.service.AuthService;
 import org.springframework.beans.BeanUtils;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
@@ -45,7 +44,7 @@ public class PasswordAuthServiceImpl implements AuthService {
         // TODO：远程调用验证码服务接口校验验证码
 
         // 账号是否存在，查询数据库
-        XcUser xcUser = xcUserMapper.selectOne(new LambdaQueryChainWrapper<XcUser>().eq(XcUser::getUsername, username));
+        XcUser xcUser = xcUserMapper.selectOne(new LambdaQueryWrapper<XcUser>().eq(XcUser::getUsername, username));
         if (xcUser == null) {
             throw new RuntimeException("账号不存在");
         }
