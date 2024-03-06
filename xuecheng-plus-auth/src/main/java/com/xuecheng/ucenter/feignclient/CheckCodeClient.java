0cf08fc3d95e810be64ad67a79d3d9994e1b5853
ronan <321393295@qq.com>
2024-03-06 23:09:25 +0800

    验证码校验功能

---
+++ xuecheng-plus-auth/src/main/java/com/xuecheng/ucenter/feignclient/CheckCodeClient.java
@@ -0,0 +1,10 @@
+package com.xuecheng.ucenter.feignclient;/**
+ * @ClassName: CheckCodeClient
+ * @Package: com.xuecheng.ucenter.feignclient
+ * @Description:
+ * @Author: Ronan
+ * @Create 2024/3/6 - 23:00
+ * @Version: v1.0
+ *
+ */public interface CheckCodeClient {
+}
---
+++ xuecheng-plus-auth/src/main/java/com/xuecheng/ucenter/feignclient/CheckCodeClientFactory.java
@@ -0,0 +1,10 @@
+package com.xuecheng.ucenter.feignclient;/**
+ * @ClassName: CheckCodeClientFactory
+ * @Package: com.xuecheng.ucenter.feignclient
+ * @Description:
+ * @Author: Ronan
+ * @Create 2024/3/6 - 23:02
+ * @Version: v1.0
+ *
+ */public class CheckCodeClientFactory {
+}
