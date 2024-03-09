177bb17c12bf42a525b2a3333045b325fa899be0
ronan <321393295@qq.com>
2024-03-09 16:18:01 +0800

    远程调用获取课程发布信息

---
+++ xuecheng-plus-learning/xuecheng-plus-learning-service/src/main/java/com/xuecheng/learning/feignclient/ContentServiceClient.java
@@ -0,0 +1,10 @@
+package com.xuecheng.learning.feignclient;/**
+ * @ClassName: ContentServiceClient
+ * @Package: com.xuecheng.learning.feignclient
+ * @Description:
+ * @Author: Ronan
+ * @Create 2024/3/9 - 16:12
+ * @Version: v1.0
+ *
+ */public class ContentServiceClient {
+}
---
+++ xuecheng-plus-learning/xuecheng-plus-learning-service/src/main/java/com/xuecheng/learning/feignclient/fallback/ContentServiceClientFallbackFactory.java
@@ -0,0 +1,10 @@
+package com.xuecheng.learning.feignclient.fallback;/**
+ * @ClassName: ContentServiceClientFallbackFactory
+ * @Package: com.xuecheng.learning.feignclient.fallback
+ * @Description:
+ * @Author: Ronan
+ * @Create 2024/3/9 - 16:14
+ * @Version: v1.0
+ *
+ */public class ContentServiceClientFallbackFactory {
+}
